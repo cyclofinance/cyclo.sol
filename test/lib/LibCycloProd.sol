f1f0d95cb0297280d9e74a7c7dccb00e0d98ce48
thedavidmeister <thedavidmeister@gmail.com>
2024-12-05 20:10:39 +0400

    prod tests for cyclo receipt

---
+++ test/lib/LibCycloProd.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 thedavidmeister
+pragma solidity ^0.8.25;
+
+address constant PROD_CYCLO_RECEIPT_ADDRESS = 0xB306F9FC8555c77FCFDb59b8F8901E97B5F02DB4;
+
+uint256 constant PROD_BLOCK_NUMBER = 34048823;
+
+string constant PROD_FORK_URL = "https://rpc.ankr.com/flare";
---
+++ test/src/concrete/receipt/CycloReceipt.prod.metadata.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 thedavidmeister
+pragma solidity =0.8.25;
+
+import {Test} from "forge-std/Test.sol";
+
+import {PROD_FORK_URL, PROD_CYCLO_RECEIPT_ADDRESS} from "test/lib/LibCycloProd.sol";
+import {CycloReceiptMetadataTest} from "./CycloReceipt.metadata.t.sol";
+
+contract CycloReceiptProdMetadataTest is CycloReceiptMetadataTest {
+    function testProdCycloReceiptURI() external {
+        vm.createSelectFork(PROD_FORK_URL);
+
+        checkCycloReceiptURI(PROD_CYCLO_RECEIPT_ADDRESS);
+    }
+
+    function testProdCycloReceiptName() external {
+        vm.createSelectFork(PROD_FORK_URL);
+
+        checkCycloReceiptName(PROD_CYCLO_RECEIPT_ADDRESS);
+    }
+
+    function testProdCycloReceiptSymbol() external {
+        vm.createSelectFork(PROD_FORK_URL);
+
+        checkCycloReceiptSymbol(PROD_CYCLO_RECEIPT_ADDRESS);
+    }
+}
