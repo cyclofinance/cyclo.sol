e70c1ee0015b975cba7faaa6f3682005f3a69160
thedavidmeister <thedavidmeister@gmail.com>
2024-12-09 17:32:35 +0400

    rm owner

--- test/prod/CycloReceipt.prod.access.t.sol
+++ test/prod/CycloReceipt.prod.access.t.sol
@@ -6,20 +6,11 @@ import {Test} from "forge-std/Test.sol";
 import {
     LibCycloProd,
     PROD_CYCLO_RECEIPT_ADDRESS,
-    PROD_CYCLO_RECEIPT_INITIAL_OWNER,
     PROD_CYCLO_VAULT_ADDRESS
 } from "test/lib/LibCycloProd.sol";
 import {IReceiptV2} from "ethgild/interface/IReceiptV2.sol";
 
 contract CycloReceiptProdAccessTest is Test {
-    function testProdCycloReceiptOwner() external {
-        LibCycloProd.createSelectFork(vm);
-
-        address owner = IReceiptV2(PROD_CYCLO_RECEIPT_ADDRESS).owner();
-
-        assertEq(owner, PROD_CYCLO_RECEIPT_INITIAL_OWNER);
-    }
-
     function testProdCycloReceiptManager() external {
         LibCycloProd.createSelectFork(vm);
 
