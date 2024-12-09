20181c8b2118ee8a3bd45c9fb5247894128a39cd
thedavidmeister <thedavidmeister@gmail.com>
2024-12-09 17:38:07 +0400

    fmt

--- test/prod/CycloReceipt.prod.access.t.sol
+++ test/prod/CycloReceipt.prod.access.t.sol
@@ -3,11 +3,7 @@
 pragma solidity =0.8.25;
 
 import {Test} from "forge-std/Test.sol";
-import {
-    LibCycloProd,
-    PROD_CYCLO_RECEIPT_ADDRESS,
-    PROD_CYCLO_VAULT_ADDRESS
-} from "test/lib/LibCycloProd.sol";
+import {LibCycloProd, PROD_CYCLO_RECEIPT_ADDRESS, PROD_CYCLO_VAULT_ADDRESS} from "test/lib/LibCycloProd.sol";
 import {IReceiptV2} from "ethgild/interface/IReceiptV2.sol";
 
 contract CycloReceiptProdAccessTest is Test {
