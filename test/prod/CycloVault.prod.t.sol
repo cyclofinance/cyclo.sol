38908ec720dc03b2d1df7d24abaf66fc2a86d744
thedavidmeister <thedavidmeister@gmail.com>
2025-11-10 18:08:33 +0400

    lint

--- test/prod/CycloVault.prod.t.sol
+++ test/prod/CycloVault.prod.t.sol
@@ -72,9 +72,9 @@ contract CycloVaultProdTest is Test {
     }
 
     function testProdCycloVaultBytecode() external view {
-        // LibCycloTestProd.checkCBORTrimmedBytecodeHashBy1167Proxy(
-        //     address(sCycloVault), address(sCycloVaultImplementation), PROD_FLARE_CYCLO_VAULT_IMPLEMENTATION_V2_CODEHASH
-        // );
+        LibCycloTestProd.checkCBORTrimmedBytecodeHashBy1167Proxy(
+            address(sCycloVault), address(sCycloVaultImplementation), PROD_FLARE_CYCLO_VAULT_IMPLEMENTATION_V2_CODEHASH
+        );
 
         LibCycloTestProd.checkCBORTrimmedBytecodeHashBy1167Proxy(
             PROD_FLARE_VAULT_CYSFLR,
