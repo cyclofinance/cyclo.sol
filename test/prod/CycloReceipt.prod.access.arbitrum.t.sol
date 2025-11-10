bb2b043bb6809a20b34a756e27de1861885bfe9c
thedavidmeister <thedavidmeister@gmail.com>
2025-11-10 19:40:02 +0400

    arbitrum access test

---
+++ test/prod/CycloReceipt.prod.access.arbitrum.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 Rain Open Source Software Ltd
+pragma solidity =0.8.25;
+
+import {Test} from "forge-std/Test.sol";
+import {PROD_ARBITRUM_VAULT_CYWETH_PYTH} from "src/lib/LibCycloProdVault.sol";
+import {PROD_ARBITRUM_RECEIPT_CYWETH_PYTH} from "src/lib/LibCycloProdReceipt.sol";
+import {LibCycloTestProd} from "test/lib/LibCycloTestProd.sol";
+import {IReceiptV2} from "ethgild/interface/deprecated/IReceiptV2.sol";
+
+contract CycloReceiptProdAccessFlareTest is Test {
+    function checkAccess(address receiptAddress, address vaultAddress, string memory asset) internal view {
+        address manager = IReceiptV2(receiptAddress).manager();
+        assertEq(manager, vaultAddress, string.concat(asset, " manager should be vault"));
+    }
+
+    function testProdCycloReceiptManagerFlare() external {
+        LibCycloTestProd.createSelectForkArbitrum(vm);
+
+        checkAccess(PROD_ARBITRUM_RECEIPT_CYWETH_PYTH, PROD_ARBITRUM_VAULT_CYWETH_PYTH, "cyWETH");
+    }
+}
